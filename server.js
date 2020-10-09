912508d624d84cb8c3040ea63f1bae105b5271f7
Henry Parrish <henryhparrish@gmail.com>
2020-10-09 18:03:05 -0500

    fixed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 const app = express();
-
+var path = require("path");
 
 app.use(logger("dev"));
 
@@ -11,12 +11,23 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+app.use(require("./routes/api-routes.js"));
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-app.use(require("./routes/api-routes.js"));
-app.use(require("./routes/html-routes.js"));  
 
 
+app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+});
+
+app.get('/exercise', (req,res) => {
+  res.sendFile(path.join(__dirname, './public/exercise.html'))
+});
+
+app.get('/stats', (req,res) => {
+  res.sendFile(path.join(__dirname, './public/stats.html'))
+});  
 
 app.listen(PORT, () => {
     console.log(`App running on port  http://localhost:${PORT}`);
