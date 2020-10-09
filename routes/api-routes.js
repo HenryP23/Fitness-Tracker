6b2228bfcc46590a7993a612218821384730f358
HenryP23 <69431847+HenryP23@users.noreply.github.com>
2020-10-09 18:16:42 -0500

    Fixed Routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,55 +1,55 @@
 const Workout = require("../models/workoutModel");
 const app = require("express").Router();
 
-  app.get("/api/workouts", (req, res) => {
-    Workout.find()
-      .then((dbWorkout) => {
-        res.json(dbWorkout);
-      })
-      .catch((err) => {
-        res.json(err);
-      });
-  });
+app.get("/api/workouts", (req, res) => {
+  Workout.find()
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
-  app.post("/api/workouts", ({ body }, res) => {
-    Workout.create(body)
-      .then((dbWorkout) => {
-        res.json(dbWorkout);
-      })
-      .catch((err) => {
-        res.json(err);
-      });
-  });
+app.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
-  app.put("/api/workouts/:id", ({ body, params }, res) => {
-    Workout.findByIdAndUpdate(
-      params.id,
-      {
-        $push: {
-          exercises: body
-        }
-      },
-      {
-        new: true
+app.put("/api/workouts/:id", ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(
+    params.id,
+    {
+      $push: {
+        exercises: body
       }
-    )
-      .then((dbWorkout) => {
-        res.json(dbWorkout);
-      })
-      .catch((err) => {
-        res.json(err);
-      });
-  });
+    },
+    {
+      new: true
+    }
+  )
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
-  app.get("/api/workouts/range", (req, res) => {
-    Workout.find({})
-      .limit(10)
-      .then((dbWorkout) => {
-        res.json(dbWorkout);
-      })
-      .catch((err) => {
-        res.json(err);
-      });
-  });
+app.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .limit(10)
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
-  module.exports = app;
+module.exports = app;
